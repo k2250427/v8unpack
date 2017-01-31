2490335327b69c29dcd157ec913fff319fe8d381
Sergey Batanov <sergey.batanov@dmpas.ru>
2017-01-31 15:49:59 +0300

    Удалён каталог `itest`.
    Предполагается использование кроссплатформенного тестирования без
    использования производных продуктов платформы 1С.

--- itest/Test 8.1.9.57 (level0, withIB).cmd
+++
@@ -1,1 +0,0 @@
-cscript.exe UPdirTest00.js "8.1.9.57" /TESTIB
--- itest/UnpackAll.bat
+++
@@ -1,3 +0,0 @@
-V8Unpack -unpack %1 %1.und
-FOR %%1 IN (%1.und\*.data) DO ..\bin\V8Unpack.exe -undeflate %%1 %%1.und
-FOR %%1 IN (%1.und\*.und) DO ..\bin\V8Unpack.exe -unpack %%1 %%1.unp
